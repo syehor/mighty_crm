a4fada216e3964f239de3cd4f1e0157370c81b91
Serhii Yehorov <syehor@softserveinc.com>
2012-08-16 09:45:13 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MightyCrm::Application.config.secret_token = '0d1786b0e792db126afa3de5a5a6626bf0571c9d137c7f68f3b83d9556fdc04359b8d282615c212ceb9609d6fa70937ee988cf16a3bfb3ae78d94c0900620a50'
